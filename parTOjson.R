5c587e1b9f29d37241c6ec3fec259ab59abbb2a7
Andrés Quiñones <36297302+andreseqp@users.noreply.github.com>
2019-11-15 16:14:40 -0500

    Changes to run simul

--- parTOjson.R
+++ parTOjson.R
@@ -10,23 +10,24 @@ fileName<-"parameters.json"
 here()
 
 
-param<-list(totGen   = 10000,   nRep    = 5,
-            printGen = 1000,   printLearn = 1000,
-            printLearnInt = 3500,
+param<-list(totGen   = 5000,   nRep    = 5,
+            printGen = 500,   printLearn = 1000,
+            printLearnInt = 1900,
             popSize  = 1000,  baselineFit = 2,   
             MutSd    = 0.3,
-            nInt        = 2000,  init     = c(0,0,1),
+            nInt        = 1000,  init     = c(0,0,1),
             mutRate  = 0.02,  mutType  = 0,
             sampleSize = 10,   strQual  = 10,
             alphaBad	 = 5,    betaBad	 = 10,
             alphaCrit  = 0.01,  alphaAct = 0.01,
             sigSq   	 = 0.01, nCenters = 6,
             initCrit = 0,      initAct=0,
-            QualStDv   = 0.2,
+            QualStDv   = 0.2, betCost = 0,
+            alphCost	 = 3,
             nIntGroup  = 1000,
             payoff_matrix = c(1.5,1,0,0.5),
-            namParam = "nIntGroup",
-            rangParam = c(10,100,1000),
+            namParam = "QualStDv",
+            rangParam = c(0.1,0.2,1.1),
             folderL=paste(here("Simulations"),"/",sep=""))
 
 apendScenar<-"Evol"
@@ -36,6 +37,8 @@ runTime<-"360:00:00"# "10:00:00"#
 partition<-"long"#"short"#
 
 memor<- "16GB"#"4096"
+
+nodes<-10
 
 param$folder=paste0("/hpcfs/home/a.quinones/BadgeStatus/",
                     param$namParam,apendScenar,"_/")
@@ -81,18 +84,18 @@ for (i in 1:1) {
       if(substr(ans, 1, 1) == "y"){
         write(outParam,paste(param$folderL,fileName,sep = ""))
         jobfile(param$folderL,paste0(param$namParam,apendScenar),timelim = runTime,
-                partition,nodes = 1,mem = memor)
+                partition,nodes = nodes,mem = memor)
       }
     }
     else{
       jobfile(param$folderL,paste0(param$namParam,apendScenar),timelim = runTime,
-              partition,nodes = 1,mem = memor)
+              partition,nodes = nodes,mem = memor)
     }
   }
   else{
     write(outParam,paste(param$folderL,fileName,sep = ""))
     jobfile(param$folderL,paste0(param$namParam,apendScenar),timelim = runTime,
-            partition,nodes = 1,mem = memor)
+            partition,nodes = nodes,mem = memor)
   }
   # system(paste(exedir,
   #   gsub("\\","/",paste(simsdir,listfolders[i],fileName,sep="\\"),fixed=TRUE)
