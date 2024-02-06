f1c9a0936156ab787e71df05387b6b243cd207ff
Andres Quinones <andresqp@gmail.com>
2024-02-06 11:30:31 -0500

    New manuscript version

--- neededPack.R
+++
@@ -1,8 +0,0 @@
-# Funtion to quickly install all the necesary packages -------------------------
-
-installProj.Packages<-function(){
-  packlist<-c("doParallel",  "parallel", "iterators","foreach","lattice",
-              "data.table", "rmarkdown","jsonlite","RColorBrewer", "here",
-              "stats","graphics", "grDevices")
-  install.packages(packlist)
-}
